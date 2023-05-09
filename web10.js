9a0d138e570a718a495164972840c5436a971195
Hemangi1123-hub <np03cs4s230013@heraldcollege.edu.np>
2023-05-09 15:23:45 +0545

    my portfolio

---
+++ web10.js
@@ -0,0 +1,44 @@
+/*let marks= prompt("Enter your marks: ");
+
+if (marks<=70){
+   alert("First class honors");
+   console.log("First class honors");
+}else if{(marks<=69 && marks>59)
+   alert("Second class honors");
+   console.log("Second class honors");
+}else if{(marks>=59 && marks=40)
+    alert("Third class honors");
+    console.log("Third class honors");
+}*/
+
+/*function num(){
+    let a = 6;
+    if (true){
+        let a =5;
+        console.log(a);
+    }
+    console.log(a);
+}
+
+function num1(){
+    var b = 7;
+    if (true0){
+        var b = 9;
+        console.log(b);
+    }
+}
+num1();*/
+/*let i ;
+for(i=1;i<=100;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}*/
+
+function multiply(a,b){
+    let multiply =1;
+    multiply =a * b;
+    console.log(multiply)
+}
+multiply(2,3);
+multiply(3,3);
